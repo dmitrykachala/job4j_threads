96f74587238981726e6adec3c918c7344645826d
dmitrykachala <kosas@mail.ru>
2021-12-06 14:09:48 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        System.out.println("Start loading ... ");
+                        for (int i = 0; i < 101; i++) {
+                            Thread.sleep(1000);
+                            System.out.println("\rLoading : " + i  + "%");
+                        }
+
+                        System.out.println("Loaded.");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+        System.out.println("Main");
+    }
+}
