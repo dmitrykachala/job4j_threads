1db7e53f53b646beea81fdc76d5595d073b6e00e
dmitrykachala <kosas@mail.ru>
2021-12-06 13:08:56 +0300

    1. Запуск нити. Thread#start() [#1016]

---
+++ README.md
@@ -0,0 +1,3 @@
+# job4j_threads
+[![Build Status](https://app.travis-ci.com/dmitrykachala/job4j_threads.svg?branch=main)](https://app.travis-ci.com/dmitrykachala/job4j_threads)
+[![codecov](https://codecov.io/gh/dmitrykachala/job4j_threads/branch/main/graph/badge.svg?token=VDXM80730Q)](https://codecov.io/gh/dmitrykachala/job4j_threads)
---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,11 @@
+package main.java.ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
