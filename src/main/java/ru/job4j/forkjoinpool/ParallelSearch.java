b1536d9193abff8de4b5b1c6a944178e94e0b8b3
dmitryu.kachala <kosas@mail.ru>
2021-12-20 00:16:47 +0300

    3. ForkJoinPool [#315067]

--- src/main/java/ru/job4j/forkjoinpool/ParallelSearch.java
+++ src/main/java/ru/job4j/forkjoinpool/ParallelSearch.java
@@ -20,36 +20,40 @@ public class ParallelSearch extends RecursiveTask<Integer> {
     @Override
     protected Integer compute() {
 
+        int rsl = -1;
+
         if (finish - start <= 10) {
-            for (int i = start; i < finish; i++) {
+            for (int i = start; i <= finish; i++) {
                 if (array[i].equals(object)) {
-                    return i;
+                    rsl = i;
                 }
             }
+            return rsl;
         }
 
         ParallelSearch leftSearch = new ParallelSearch(array, object,
-                0, array.length / 2);
+                start, array.length / 2);
         ParallelSearch rightSearch = new ParallelSearch(array, object,
-                1 + array.length / 2, array.length - 1);
+                1 + array.length / 2, finish);
 
         leftSearch.fork();
         rightSearch.fork();
 
-        return leftSearch.join() + rightSearch.join();
+        return Math.max(leftSearch.join(), rightSearch.join());
     }
 
     public Integer search() {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         Integer rsl = forkJoinPool.invoke(new ParallelSearch(array, object,
-                0, array.length - 1));
+                start, finish));
         System.out.println(rsl);
         return rsl;
     }
 
     public static void main(String[] args) {
         ParallelSearch ps = new ParallelSearch(new Integer[]{12, 124, 4, 346, 674,
-                234, 34, 7, 673, 42, 3}, 7, 0, 10);
+                234, 34, 7, 673, 42, 3, 7, 9, 0, 878, 8, 54, 989, 3244523, 7,
+                7}, 7, 0, 20);
         ps.search();
     }
 }
--- src/test/java/ru/job4j/forkjoinpool/ParallelSearchTest.java
+++ src/test/java/ru/job4j/forkjoinpool/ParallelSearchTest.java
@@ -15,4 +15,22 @@ public class ParallelSearchTest {
         assertThat(rsl, is(7));
     }
 
+    @Test
+    public void whenTestPSNotFound() {
+        ParallelSearch ps = new ParallelSearch(new Integer[]{12, 124, 4, 346, 674,
+                234, 34, 7, 673, 42, 3, 7, 9, 0, 878, 8, 54, 989, 3244523, 7,
+                7}, -5, 0, 20);
+        int rsl = ps.search();
+        assertThat(rsl, is(-1));
+    }
+
+    @Test
+    public void whenTestPSAndFoundLast() {
+        ParallelSearch ps = new ParallelSearch(new Integer[]{12, 124, 4, 346, 674,
+                234, 34, 7, 673, 42, 3, 7, 9, 0, 878, 8, 54, 989, 3244523, 7,
+                7}, 7, 0, 20);
+        int rsl = ps.search();
+        assertThat(rsl, is(20));
+    }
+
 }
